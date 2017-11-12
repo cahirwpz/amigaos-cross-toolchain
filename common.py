add3660e0d4889fcd913f2248bfe14c56c285744
Krystian Bacławski <krystian.baclawski@gmail.com>
2017-11-12 17:14:26 +0100

    Gcc parallel build fails as well :(

--- common.py
+++ common.py
@@ -421,7 +421,7 @@ def fetch(name, url):
     else:
       info('File "%s" already downloaded.', name)
   elif url.startswith('svn'):
-    execute('svn', 'export', '--quiet', url, name)
+    execute('svn', 'export', url, name)
   elif url.startswith('git'):
     if not path.exists(name):
       execute('git', 'clone', url, name)
